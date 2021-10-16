37424e13a69804ba0dca3ccf2b6e9f6f9f8421ba
Frederik Batuna <fbatuna@key-biz.it>
2021-10-16 04:04:50 +0200

    add: material ui nav bar
    Co-authored-by: Alice Cecchetti <mialis@users.noreply.github.com>

---
+++ dev.webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const config = require("./webpack.config");
+
+module.exports = {
+  ...config,
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    client: {
+      overlay: { errors: true, warnings: false },
+    },
+    hot: true,
+  },
+  module: {
+    ...config.module,
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [require("react-refresh/babel")],
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      ...config.module.rules,
+    ],
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
+};
