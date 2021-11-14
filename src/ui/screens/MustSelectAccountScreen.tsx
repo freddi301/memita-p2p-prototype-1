7cd56783f460e4f4ad6bb8f587523827437d0fab
Frederik Batuna <fbatuna@key-biz.it>
2021-11-14 03:22:04 +0100

    add: shortcuts to must select account

--- src/ui/screens/MustSelectAccountScreen.tsx
+++ src/ui/screens/MustSelectAccountScreen.tsx
@@ -3,23 +3,84 @@ import { NavigationContext } from "../NavigationStack";
 import { HeaderContentControlsLayout } from "../components/HeaderContentControlsLayout";
 import { ButtonGroup } from "../components/ButtonGroup";
 import { Button } from "../components/Button";
-import { Icon } from "../components/Icon";
 import { Text } from "../components/Text";
+import { Virtuoso } from "react-virtuoso";
+import { FrontendFacade } from "../../logic/FrontendFacade";
+import { StyleContext } from "../StyleProvider";
+import { Clickable } from "../components/Clickable";
+import { css } from "styled-components/macro";
+import { EmptyListPlaceholder } from "../components/EmptyListPlaceholder";
+import { SimpleHeader } from "../components/SimpleHeader";
 
-export function MustSelectAccountScreen() {
+type MustSelectAccountScreenProps = {
+  onUse(accountPublicKey: string): void;
+};
+export function MustSelectAccountScreen({ onUse }: MustSelectAccountScreenProps) {
   const navigationStack = React.useContext(NavigationContext);
   const onAccounts = () => {
     navigationStack.push({ screen: "account-list" });
   };
+  const accountCount = FrontendFacade.useAccountListSize() ?? 0;
+  const onAccount = (publicKey: string) => {
+    onUse(publicKey);
+  };
+  const onCreate = () => {
+    navigationStack.push({ screen: "create-account" });
+  };
   return (
     <HeaderContentControlsLayout
-      header={<Text text="Need to select account" color="primary" weight="bold" size="big" />}
-      content={null}
+      header={
+        <SimpleHeader>
+          <Text text="You must select an account" color="primary" weight="bold" size="big" />
+        </SimpleHeader>
+      }
+      content={
+        !accountCount ? (
+          <EmptyListPlaceholder>No accounts</EmptyListPlaceholder>
+        ) : (
+          <Virtuoso
+            style={{ height: "100%" }}
+            totalCount={accountCount}
+            itemContent={(index) => <AccountItem index={index} onAccount={onAccount} />}
+          />
+        )
+      }
       controls={
         <ButtonGroup>
-          <Button icon={<Icon icon="Account" />} enabled={true} label="Accounts" onClick={onAccounts} />
+          <Button icon="Account" enabled={true} label="Accounts" onClick={onAccounts} showLabel={false} />
+          <Button icon="Create" enabled={true} label="Create" onClick={onCreate} showLabel={true} />
         </ButtonGroup>
       }
     />
+  );
+}
+
+type AccountItemProps = {
+  index: number;
+  onAccount(publicKey: string): void;
+};
+function AccountItem({ index, onAccount }: AccountItemProps) {
+  const { theme } = React.useContext(StyleContext);
+  const account = FrontendFacade.useAccountListAtIndex(index);
+  return (
+    <Clickable
+      onClick={() => {
+        if (account) {
+          onAccount(account.publicKey);
+        }
+      }}
+    >
+      <div
+        css={css`
+          display: grid;
+          grid-auto-flow: row;
+          grid-auto-columns: auto;
+          padding: ${theme.spacing.text.vertical} ${theme.spacing.text.horizontal};
+        `}
+      >
+        <Text color="primary" size="normal" weight="bold" text={account?.name ?? "..."} />
+        <Text color="secondary" size="normal" weight="normal" text={account?.publicKey ?? "..."} />
+      </div>
+    </Clickable>
   );
 }
