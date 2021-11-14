225649b320085cffa153ebd2cc7491e73034e002
Frederik Batuna <fbatuna@key-biz.it>
2021-11-14 03:28:11 +0100

    add: hint blocks to home screen

--- src/ui/screens/HomeScreen.tsx
+++ src/ui/screens/HomeScreen.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Text } from "../components/Text";
-import { Icon } from "../components/Icon";
 import { HeaderContentControlsLayout } from "../components/HeaderContentControlsLayout";
 import { Button } from "../components/Button";
 import { ButtonGroup } from "../components/ButtonGroup";
 import { NavigationContext } from "../NavigationStack";
+import { FrontendFacade } from "../../logic/FrontendFacade";
+import { css } from "styled-components/macro";
+import { StyleContext } from "../StyleProvider";
 
 type HomeScreenProps = {};
 export function HomeScreen(props: HomeScreenProps) {
+  const { theme } = React.useContext(StyleContext);
   const navigationStack = React.useContext(NavigationContext);
+  const accountCount = FrontendFacade.useAccountListSize();
+  const contactsCount = FrontendFacade.useContactListSize();
   const onContacts = () => {
     navigationStack.push({ screen: "contact-list" });
   };
@@ -20,15 +25,98 @@ export function HomeScreen(props: HomeScreenProps) {
   };
   return (
     <HeaderContentControlsLayout
-      header={<Text text="Home" color="primary" weight="bold" size="big" />}
-      content={null}
+      header={null}
+      content={
+        <div
+          css={css`
+            display: grid;
+            grid-auto-flow: row;
+            grid-auto-rows: auto;
+            grid-row-gap: ${theme.spacing.gap};
+          `}
+        >
+          {accountCount === 0 && <CreateAccountHint />}
+          {contactsCount === 0 && <AddContactHint />}
+        </div>
+      }
       controls={
         <ButtonGroup>
-          <Button icon={<Icon icon="Conversations" />} enabled={true} label="Conversations" onClick={onConversations} />
-          <Button icon={<Icon icon="Contacts" />} enabled={true} label="Contacts" onClick={onContacts} />
-          <Button icon={<Icon icon="Account" />} enabled={true} label="Accounts" onClick={onAccounts} />
+          <Button
+            icon="Conversations"
+            enabled={true}
+            label="Conversations"
+            onClick={onConversations}
+            showLabel={false}
+          />
+          <Button icon="Contacts" enabled={true} label="Contacts" onClick={onContacts} showLabel={false} />
+          <Button icon="Account" enabled={true} label="Accounts" onClick={onAccounts} showLabel={false} />
         </ButtonGroup>
       }
     />
+  );
+}
+
+function CreateAccountHint() {
+  const { theme } = React.useContext(StyleContext);
+  const navigationStack = React.useContext(NavigationContext);
+  const onCreate = () => {
+    navigationStack.push({ screen: "create-account" });
+  };
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-row-gap: ${theme.spacing.text.vertical};
+        grid-auto-flow: row;
+        grid-auto-rows: auto;
+        padding: ${theme.spacing.text.vertical} ${theme.spacing.text.horizontal};
+      `}
+    >
+      <div>
+        <Text color="primary" size="normal" weight="normal" text="You don't have any accounts on this device" />
+      </div>
+      <div
+        css={css`
+          display: grid;
+          grid-auto-flow: column;
+          grid-column-gap: ${theme.spacing.text.horizontal};
+        `}
+      >
+        <Button label="Login" icon="Import" onClick={() => {}} enabled={false} showLabel={true} />
+        <Button label="Signup" icon="Create" onClick={onCreate} enabled={true} showLabel={true} />
+      </div>
+    </div>
+  );
+}
+
+function AddContactHint() {
+  const { theme } = React.useContext(StyleContext);
+  const navigationStack = React.useContext(NavigationContext);
+  const onCreate = () => {
+    navigationStack.push({ screen: "create-contact" });
+  };
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-row-gap: ${theme.spacing.text.vertical};
+        grid-auto-flow: row;
+        grid-auto-rows: auto;
+        padding: ${theme.spacing.text.vertical} ${theme.spacing.text.horizontal};
+      `}
+    >
+      <div>
+        <Text color="primary" size="normal" weight="normal" text="You don't have any contacts on this device" />
+      </div>
+      <div
+        css={css`
+          display: grid;
+          grid-auto-flow: column;
+          grid-column-gap: ${theme.spacing.text.horizontal};
+        `}
+      >
+        <Button label="AddContact" icon="Create" onClick={onCreate} enabled={true} showLabel={true} />
+      </div>
+    </div>
   );
 }
